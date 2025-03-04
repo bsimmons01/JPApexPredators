4b4fa2a741ba9cfce8779a0337c9a220c21cd884
Brian Simmons <bsimmons01@gmail.com>
2025-03-04 11:34:30 -0700

    List modifications to show JSON data in table view

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -5,19 +5,42 @@
 //  Created by Brian Simmons on 3/4/25.
 //
 
-struct ApexPredator: Decodable {
+import SwiftUI
+
+struct ApexPredator: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum APType: String, Decodable {
+        case land // "land"
+        case air  // "air"
+        case sea  // "sea"
+        
+        var background: Color {
+            switch self {
+            case .land:
+                .brown
+            case .air:
+                .teal
+            case .sea:
+                .blue
+            }
+        }
     }
 }
