7258803ca548098028452aea76e96d4b88c46f30
Brian Simmons <bsimmons01@gmail.com>
2025-03-04 13:21:30 -0700

    Navigation and Search added

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -10,31 +10,56 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(predators.apexPredators) { predator in
-            //Text(predator.name)
-            HStack {
-                // Dino image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                VStack(alignment: .leading) {
-                    // Dino name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    // Dino type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+        NavigationStack {
+            List(filteredDinos) { predator in
+                //Text(predator.name)
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        // Dino image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        VStack(alignment: .leading) {
+                            // Dino name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Dino type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
                 }
             }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
         .preferredColorScheme(.dark)
     }
