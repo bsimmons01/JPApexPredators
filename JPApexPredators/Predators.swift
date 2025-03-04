c8d8f3dc2b5389474fc51bcc56e715406bd0939e
Brian Simmons <bsimmons01@gmail.com>
2025-03-04 11:09:34 -0700

    Build Predators Class

---
+++ JPApexPredators/Predators.swift
@@ -0,0 +1,30 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Brian Simmons on 3/4/25.
+//
+
+import Foundation
+
+class Predators {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
