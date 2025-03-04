40d8b6b391084b88c3fe8dfc695a2a3f3336628a
Brian Simmons <bsimmons01@gmail.com>
2025-03-04 15:19:12 -0700

    Gradients on image

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -18,6 +18,10 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(stops: [Gradient.Stop(color: .clear, location: 0.8),
+                                                   Gradient.Stop(color: .black, location: 1)], startPoint: .top, endPoint: .bottom)
+                        }
                     
                     // Dino image
                     Image(predator.image)
@@ -78,6 +82,6 @@ struct PredatorDetail: View {
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().apexPredators[2])
-//        .preferredColorScheme(.dark)
+    PredatorDetail(predator: Predators().apexPredators[10])
+        .preferredColorScheme(.dark)
 }
